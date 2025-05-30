a130fb8edf93177a622f5c6503d10fbd9b32550f
gitxandert <alexandertoth96@yahoo.com>
2025-05-29 21:09:38 -0400

    remove margins

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <Router basename={import.meta.env.BASE_URL}>
-      <div className="min-h-dvh flex flex-col bg-cyan-400 dark:bg-rose-950 change-color">
+      <div className="min-h-dvh w-full min-w-0 flex flex-col bg-cyan-400 dark:bg-rose-950 change-color">
+
         {/* NAVBAR → always at top, sticky if you scroll */}
         <Navbar />
         {/* MAIN CONTENT → grows to fill remaining space */}
-        <main className="flex-1 w-full mx-auto p-6 pt-12 overflow-hidden">
+        <main className="flex-1 w-full p-6 pt-12 overflow-hidden">
           <AppRoutes />
         </main>
         <Footer />

a9bb528f8525ec5d46097277511041bad75ce170
gitxandert <alexandertoth96@yahoo.com>
2025-05-29 21:03:05 -0400

    edit style to w-full

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <Router basename={import.meta.env.BASE_URL}>
-      <div className="min-h-screen flex flex-col bg-cyan-400 dark:bg-rose-950 transition-colors duration-600 ease-out">
+      <div className="min-h-dvh flex flex-col bg-cyan-400 dark:bg-rose-950 change-color">
         {/* NAVBAR → always at top, sticky if you scroll */}
         <Navbar />
         {/* MAIN CONTENT → grows to fill remaining space */}
-        <main className="flex-1 container mx-auto p-6 pt-12 overflow-hidden">
+        <main className="flex-1 w-full mx-auto p-6 pt-12 overflow-hidden">
           <AppRoutes />
         </main>
         <Footer />

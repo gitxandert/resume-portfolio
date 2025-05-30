2478c468bbb9f6482a1d80aa099a639ed13b2fc5
gitxandert <alexandertoth96@yahoo.com>
2025-05-29 22:16:34 -0400

    add w-full to footer idk

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-12 py-6 text-center text-sm text-blue-950 dark:text-rose-200 change-color">
+    <footer className="w-full mt-12 py-6 text-center text-sm text-blue-950 dark:text-rose-200 change-color">
       © {new Date().getFullYear()} Alexander Toth — built with React & Vite
     </footer>
   );
